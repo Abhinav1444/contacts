be612b1b6efcded215c10b7e93ba617a8cde1312
Abhinav <abhinav1444.be20@chitkara.edu.in>
2022-09-27 20:08:34 +0530

    UI complete

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React, {useState, useEffect} from 'react';
+import './App.css';
+import ContactCard from './ContactCard';
+const App=()=>{
+  const[results, setResults]= useState([]);
+  
+  useEffect(()=>{
+    fetch("https://randomuser.me/api/?results=5")
+    .then(response=>response.json())
+    .then(data=>{
+      console.log(data)
+      setResults(data.results)
+    });
+  }, [])
+  
+
+  return( 
+    <div id='container'>
+    <h1>Contacts</h1>
+    {results.map((result, index)=>{
+      return(
+        <ContactCard key={index}
+        avatarUrl={result.picture.large}
+      name={result.name.first}
+      email={result.email}
+      age={result.dob.age}/>
+      )
+    })}
+    </div>
+  )
+}
+
+export default App
